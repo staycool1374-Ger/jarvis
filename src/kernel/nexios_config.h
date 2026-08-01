4caaced657ac870507fed03ebd1c0ea103ac9ba5
Arnold Hasshold <staycool@arnoldhasshold.de>
2026-08-01 22:51:54 +0200

    bump: version to v0.3.6-dev (fix PATCH=6, align Doxyfile + ROADMAP)

--- src/kernel/nexios_config.h
+++ src/kernel/nexios_config.h
@@ -15,10 +15,10 @@
 // ---------------------------------------------------------------------------
 // Version
 // ---------------------------------------------------------------------------
-#define CONFIG_VERSION "0.3.6"
+#define CONFIG_VERSION "0.3.6-dev"
 #define CONFIG_VERSION_MAJOR 0
 #define CONFIG_VERSION_MINOR 3
-#define CONFIG_VERSION_PATCH 5
+#define CONFIG_VERSION_PATCH 6
 #define CONFIG_VERSION_NUM ((CONFIG_VERSION_MAJOR << 16) | \
                             (CONFIG_VERSION_MINOR << 8) |  \
                             CONFIG_VERSION_PATCH)
